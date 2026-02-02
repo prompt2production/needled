eeb893f1d8b291a72d0db7255eb984656a2a0f43
chrisongithub1977 <chrisroweonline@gmail.com>
2026-02-02 21:36:38 +0000

    apk updated version to build 2

--- src/config/tester.ts
+++ src/config/tester.ts
@@ -1,6 +1,6 @@
 export const testerConfig = {
-  apkDownloadUrl: 'https://expo.dev/accounts/consulcode/projects/needled-mobile/builds/22f94fe2-54bc-4703-b6a7-4a507af1c389',
-  qrCodePath: '/assets/apk_qr.png',
+  apkDownloadUrl: 'https://expo.dev/accounts/consulcode/projects/needled-mobile/builds/9b69a56d-be7a-4d24-adbe-6b89170ed1d0',
+  qrCodePath: '/assets/apk_qr_b2.png',
   version: '1.0.0-beta',
   minAndroidVersion: '8.0 (Oreo)',
 }
